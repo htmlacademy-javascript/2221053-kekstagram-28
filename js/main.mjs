ae5e6222e7ff82b5d46d11bbf13652243d112ee3
Bebnev85 <andre44it@gmail.com>
2023-03-16 23:35:16 +0300

    Произведено переименование функций и констант в файле modalphotosee.mjs

--- js/main.mjs
+++ js/main.mjs
@@ -1,9 +1,9 @@
 import { generatePhotos } from './mocks/generate.mjs';
 import { renderUserPhotos } from './render-photos.mjs';
-import { showModalPhotoSee } from './modalfhotosee.mjs';
+import { addedHandlerPhotomodalWindow } from './modalfhotosee.mjs';
 import { PHOTO_COUNT } from './mocks/const.mjs';
 
 const photos = generatePhotos(PHOTO_COUNT);
 
 renderUserPhotos(photos);
-showModalPhotoSee(photos);
+addedHandlerPhotomodalWindow(photos);
--- js/modalfhotosee.mjs
+++ js/modalfhotosee.mjs
@@ -1,10 +1,9 @@
-//import { photos } from './main.mjs';
 import { isEsc, isEnter } from './utils.mjs';
 
 const COMMENT_COUNT_ADDED = 5;
-const MODAL = document.querySelector('.big-picture');
+const modal = document.querySelector('.big-picture');
 
-let commentsCountSee;
+let numberCommentsDisplayed;
 let photoData = null;
 
 const renderComment = (comment) => {
@@ -29,13 +28,13 @@ const renderComment = (comment) => {
 const renderCommentList = (comments) => {
   const elementCommentsList = document.querySelector('.social__comments');
   if (comments.length > 0) {
-    for (let i = commentsCountSee; i < commentsCountSee + Math.min(COMMENT_COUNT_ADDED, comments.length - commentsCountSee); i++) {
+    for (let i = numberCommentsDisplayed; i < numberCommentsDisplayed + Math.min(COMMENT_COUNT_ADDED, comments.length - numberCommentsDisplayed); i++) {
       elementCommentsList.appendChild(renderComment(comments[i]));
     }
   }
-  commentsCountSee += Math.min(COMMENT_COUNT_ADDED, comments.length - commentsCountSee);
-  MODAL.querySelector('.social__comment-count').firstChild.nodeValue = `${commentsCountSee} из `;
-  MODAL.querySelector('.comments-count').textContent = comments.length;
+  numberCommentsDisplayed += Math.min(COMMENT_COUNT_ADDED, comments.length - numberCommentsDisplayed);
+  modal.querySelector('.social__comment-count').firstChild.nodeValue = `${numberCommentsDisplayed} из `;
+  modal.querySelector('.comments-count').textContent = comments.length;
 };
 
 function onButtonLoadCommentsClick() {
@@ -49,38 +48,38 @@ function onButtonLoadCommentsEnterKeydown(evt) {
   }
 }
 
-const closeModalWindowPhoto = () => {
+const closemodalWindowPhoto = () => {
   const elementsPhoto = document.querySelectorAll('.picture');
-  MODAL.classList.add('hidden');
-  MODAL.querySelector('.social__comments-loader').removeEventListener('click', onButtonLoadCommentsClick);
-  MODAL.querySelector('.social__comments-loader').removeEventListener('keydown', onButtonLoadCommentsEnterKeydown);
-  document.removeEventListener('keydown', onModalPhotoWindowEscKeydown);
-  MODAL.querySelector('.big-picture__cancel').removeEventListener('click', onButtonCloseModalPhotoWindowClick);
-  MODAL.querySelector('.big-picture__cancel').removeEventListener('keydown', onButtonCloseModalPhotoWindowEnterKeydown);
+  modal.classList.add('hidden');
+  modal.querySelector('.social__comments-loader').removeEventListener('click', onButtonLoadCommentsClick);
+  modal.querySelector('.social__comments-loader').removeEventListener('keydown', onButtonLoadCommentsEnterKeydown);
+  document.removeEventListener('keydown', onmodalPhotoWindowEscKeydown);
+  modal.querySelector('.big-picture__cancel').removeEventListener('click', onButtonClosemodalPhotoWindowClick);
+  modal.querySelector('.big-picture__cancel').removeEventListener('keydown', onButtonClosemodalPhotoWindowEnterKeydown);
   for (const element of elementsPhoto) {
     element.tabIndex = -1;
   }
 };
 
-function onModalPhotoWindowEscKeydown(evt) {
+function onmodalPhotoWindowEscKeydown(evt) {
   if (isEsc(evt.key)) {
     evt.preventDefault();
-    closeModalWindowPhoto();
+    closemodalWindowPhoto();
   }
 }
 
-function onButtonCloseModalPhotoWindowEnterKeydown(evt) {
+function onButtonClosemodalPhotoWindowEnterKeydown(evt) {
   if (isEnter(evt.key)) {
     evt.preventDefault();
-    closeModalWindowPhoto();
+    closemodalWindowPhoto();
   }
 }
 
-function onButtonCloseModalPhotoWindowClick() {
-  closeModalWindowPhoto();
+function onButtonClosemodalPhotoWindowClick() {
+  closemodalWindowPhoto();
 }
 
-const ShowModalPhotoWindow = (photoElement, photos) => {
+const ShowmodalPhotoWindow = (photoElement, photos) => {
   const id = photoElement.getAttribute('data-id');
   photoData = photos.find((item) => item.id === +id);
 
@@ -89,37 +88,37 @@ const ShowModalPhotoWindow = (photoElement, photos) => {
     element.tabIndex = -1;
   }
 
-  commentsCountSee = 0;
+  numberCommentsDisplayed = 0;
 
-  MODAL.querySelector('.social__comments').remove();
-  MODAL.querySelector('.social__comment-count').insertAdjacentHTML('afterend', '<ul class="social__comments"></ul>');
+  modal.querySelector('.social__comments').remove();
+  modal.querySelector('.social__comment-count').insertAdjacentHTML('afterend', '<ul class="social__comments"></ul>');
 
-  MODAL.querySelector('.big-picture__img').querySelector('img').src = photoData.url;
-  MODAL.querySelector('.likes-count').textContent = photoData.likes;
+  modal.querySelector('.big-picture__img').querySelector('img').src = photoData.url;
+  modal.querySelector('.likes-count').textContent = photoData.likes;
   renderCommentList(photoData.comments);
 
-  MODAL.querySelector('.big-picture__cancel').addEventListener('click', onButtonCloseModalPhotoWindowClick);
-  document.addEventListener('keydown', onModalPhotoWindowEscKeydown);
-  MODAL.querySelector('.big-picture__cancel').addEventListener('keydown', onButtonCloseModalPhotoWindowEnterKeydown);
-  MODAL.querySelector('.social__comments-loader').addEventListener('click', onButtonLoadCommentsClick);
-  MODAL.querySelector('.social__comments-loader').addEventListener('keydown', onButtonLoadCommentsEnterKeydown);
-  MODAL.classList.remove('hidden');
+  modal.querySelector('.big-picture__cancel').addEventListener('click', onButtonClosemodalPhotoWindowClick);
+  document.addEventListener('keydown', onmodalPhotoWindowEscKeydown);
+  modal.querySelector('.big-picture__cancel').addEventListener('keydown', onButtonClosemodalPhotoWindowEnterKeydown);
+  modal.querySelector('.social__comments-loader').addEventListener('click', onButtonLoadCommentsClick);
+  modal.querySelector('.social__comments-loader').addEventListener('keydown', onButtonLoadCommentsEnterKeydown);
+  modal.classList.remove('hidden');
 };
 
-const showModalPhotoSee = (photos) => {
+const addedHandlerPhotomodalWindow = (photos) => {
   const elementPhotos = document.querySelector('.pictures');
   elementPhotos.addEventListener('click', (evt) => {
     if (evt.target.classList.contains('picture__img')) {
       evt.preventDefault();
-      ShowModalPhotoWindow(evt.target, photos);
+      ShowmodalPhotoWindow(evt.target, photos);
     }
   });
   elementPhotos.parentElement.addEventListener('keydown', (evt) => {
     if (isEnter(evt.key)) {
       evt.preventDefault();
-      ShowModalPhotoWindow(evt.target.querySelector('.picture__img'), photos);
+      ShowmodalPhotoWindow(evt.target.querySelector('.picture__img'), photos);
     }
   });
 };
 
-export { showModalPhotoSee };
+export { addedHandlerPhotomodalWindow };
