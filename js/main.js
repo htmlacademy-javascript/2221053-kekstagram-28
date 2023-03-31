b8d4114fba56b145d87b35a53cf39dab746b9481
Bebnev85 <andre44it@gmail.com>
2023-03-31 12:01:37 +0300

    Добавляет функцию выода сообщения об ошибке.

--- js/main.js
+++ js/main.js
@@ -1,19 +1,20 @@
 import { renderUserPhotos } from './render-photos.mjs';
 import { addHandlerPhotosElment } from './modal-window-photo-view.mjs';
+import { viewErrorMessage } from './utils.mjs';
 
 fetch('https://28.javascript.pages.academy/kekstagram/data')
   .then((response) => {
-    response.json();
-    if (!response.ok) {
-      const err = new Error('Ошибка получения даных.');
-      throw err;
+    if (response.ok) {
+      return response.json();
     }
+    const err = new Error('Ошибка получения даных.');
+    throw err;
   })
   .then((photosLoad) => {
     renderUserPhotos(photosLoad);
     addHandlerPhotosElment(photosLoad);
   })
   .catch((err) => {
-    alert(err);
+    viewErrorMessage(err);
   });
 
