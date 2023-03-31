ad87e1705aa61834f644f148e0ca65dc512c719d
Bebnev85 <andre44it@gmail.com>
2023-03-31 10:18:47 +0300

    Добавляет выод сообщения посредством alert при загрузке данных

--- js/main.js
+++ js/main.js
@@ -2,9 +2,18 @@ import { renderUserPhotos } from './render-photos.mjs';
 import { addHandlerPhotosElment } from './modal-window-photo-view.mjs';
 
 fetch('https://28.javascript.pages.academy/kekstagram/data')
-  .then((response) => response.json())
+  .then((response) => {
+    response.json();
+    if (!response.ok) {
+      const err = new Error('Ошибка получения даных.');
+      throw err;
+    }
+  })
   .then((photosLoad) => {
     renderUserPhotos(photosLoad);
     addHandlerPhotosElment(photosLoad);
+  })
+  .catch((err) => {
+    alert(err);
   });
 
